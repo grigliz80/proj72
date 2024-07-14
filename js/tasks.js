171136930117f5d980c9d60144ffb3b9e9193459
Gringo <grigoriylizanets@gmail.com>
2024-07-14 22:43:39 +0300

    created file tasks.js and first task done

---
+++ js/tasks.js
@@ -0,0 +1,23 @@
+//--------------------------------First task-----------------------------------------------------------------------------------
+
+// const numberOfFilms = +prompt("How much films did you already watch?", "");
+
+// let personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt("One of last film have you seen?", ""),
+//       b = +prompt("You assesment for it?"),
+//       c = prompt("One more of last film have you seen?", ""),
+//       d = +prompt("And assesment for it?", "");
+
+// personalMovieDB.movies[a] = b;      
+// personalMovieDB.movies[c] = d;      
+
+// console.log(personalMovieDB);
+
+//--------------------------------------------------------------------------------------------------------------------------------
